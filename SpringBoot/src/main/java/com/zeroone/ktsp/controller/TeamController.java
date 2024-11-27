827e9402d97c36505b38f4c31262a899ccb334b1
SeungWook_Lee <leesw_0924@icloud.com>
2024-11-27 22:44:50 +0900

    컨트롤러 분리화 완료

--- SpringBoot/src/main/java/com/zeroone/ktsp/controller/TeamController.java
+++ SpringBoot/src/main/java/com/zeroone/ktsp/controller/TeamController.java
@@ -6,7 +6,6 @@ import com.zeroone.ktsp.domain.Board;
 import com.zeroone.ktsp.domain.Team;
 import com.zeroone.ktsp.domain.User;
 import com.zeroone.ktsp.domain.Waiting;
-import com.zeroone.ktsp.enumeration.BoardType;
 import com.zeroone.ktsp.service.BoardService;
 import com.zeroone.ktsp.service.TeamService;
 import com.zeroone.ktsp.service.WaitingService;
@@ -32,34 +31,15 @@ public class TeamController
     private final TeamService teamService;
     private final WaitingService waitingService;
 
-    @PostMapping("/join/{boardId}")
-    public ResponseEntity<String> joinTeam(@PathVariable Long boardId, @RequestBody JoinTeamDTO joinTeamDTO, HttpSession session)
-    {
-        User user = (User) session.getAttribute("user");
-
-        String content = joinTeamDTO.getContent();
-        if (content == null || content.isBlank()) return ResponseEntity.badRequest().body("내용을 입력해야 합니다.");
-
-        Optional<Board> findBoard = boardService.findById(boardId);
-        if (findBoard.isEmpty()) return ResponseEntity.badRequest().body("지원하려는 게시글을 찾을 수 없습니다.");
-
-        Board board = findBoard.get();
-
-        if (waitingService.existsByBoardAndUser(board, user)) return ResponseEntity.badRequest().body("이미 지원하셨습니다. 지원 내역 및 결과는 마이페이지에서 확인할 수 있습니다."); // 이미 지원했는지 확인
-
-        waitingService.save(board, user, content);
-        return ResponseEntity.ok("지원이 성공적으로 완료되었습니다.");
-    }
-
     @GetMapping("/manage/{id}")
-    public String showManageView(@PathVariable long id, @RequestParam BoardType type, HttpSession session, Model model)
+    public String showManageView(@PathVariable long id, HttpSession session, Model model, @RequestParam String sidebarType, @RequestParam String boardType)
     {
         User user = (User) session.getAttribute("user");
         Optional<Board> findBoard = boardService.findById(id);
 
-        if(findBoard.isEmpty()) return "redirect:/" + type; //게시글이 존재하지 않을 때 리다이렉트
+        if(findBoard.isEmpty()) return "redirect:/" + boardType; //게시글이 존재하지 않을 때 리다이렉트
         Board board = findBoard.get();
-        if(user.getId() != board.getUser().getId()) return "redirect:/" + type; //본인이 아닌 다른 사람이 접근했을 때 리다이렉트
+        if(user.getId() != board.getUser().getId()) return "redirect:/" + boardType; //본인이 아닌 다른 사람이 접근했을 때 리다이렉트
 
         List<ManagementDTO> waitingList = new ArrayList<>();
         List<Waiting> allWaitings = waitingService.findAllByBoard(board);
@@ -96,48 +76,73 @@ public class TeamController
 
         model.addAttribute("waitingList", waitingList);
         model.addAttribute("teamList", teamList);
-        model.addAttribute("currentMenu", "one");
-        return "learning_core/management";
+        model.addAttribute("id", id);
+        model.addAttribute("sidebarType", sidebarType);
+        model.addAttribute("currentMenu", boardType);
+        return "team_management";
+    }
+
+    @PostMapping("/join/{boardId}")
+    public ResponseEntity<String> joinTeam(@PathVariable Long boardId, @RequestBody JoinTeamDTO joinTeamDTO, HttpSession session)
+    {
+        User user = (User) session.getAttribute("user");
+
+        String content = joinTeamDTO.getContent();
+        if (content == null || content.isBlank()) return ResponseEntity.badRequest().body("내용을 입력해야 합니다.");
+
+        Optional<Board> findBoard = boardService.findById(boardId);
+        if (findBoard.isEmpty()) return ResponseEntity.badRequest().body("지원하려는 게시글을 찾을 수 없습니다.");
+
+        Board board = findBoard.get();
+
+        if (waitingService.existsByBoardAndUser(board, user)) return ResponseEntity.badRequest().body("이미 지원하셨습니다. 지원 내역 및 결과는 마이페이지에서 확인할 수 있습니다."); // 이미 지원했는지 확인
+
+        waitingService.save(board, user, content);
+        return ResponseEntity.ok("지원이 성공적으로 완료되었습니다.");
     }
 
     @PostMapping("/approve/{id}")
-    public String waitingApprove(@PathVariable long id, Model model)
+    public String waitingApprove(@PathVariable long id, Model model, @RequestParam String sidebarType, @RequestParam String boardType)
     {
         Optional<Waiting> findWaiting = waitingService.findByID(id);
-        if(findWaiting.isEmpty()) return "redirect:/learning_core/mentor";
+        if(findWaiting.isEmpty()) return "redirect:/team/manage/" + id;
 
         Waiting waiting = findWaiting.get();
         teamService.save(waiting.getBoard(), waiting.getUser());
         waitingService.delete(waiting);
 
         model.addAttribute("boardID", waiting.getBoard().getId());
-        model.addAttribute("currentMenu", "one");
-        return "redirect:/learning_core/mentor/manage/" + waiting.getBoard().getId();
+        model.addAttribute("sidebarType", sidebarType);
+        model.addAttribute("currentMenu", boardType);
+        return "redirect:/team/manage/" + waiting.getBoard().getId() + "?sidebarType=" + sidebarType + "&boardType=" + boardType;
     }
 
     @PostMapping("/reject/{id}")
-    public String teamReject(@PathVariable long id, Model model)
+    public String teamReject(@PathVariable long id, Model model, @RequestParam String sidebarType, @RequestParam String boardType)
     {
         Optional<Waiting> findWaiting = waitingService.findByID(id);
-        if(findWaiting.isEmpty()) return "redirect:/learning_core/mentor";
+        if(findWaiting.isEmpty()) return "redirect:/" + boardType;
 
         Waiting waiting = findWaiting.get();
         waitingService.rejectWaiting(waiting);
+        log.info("거절됨 : {} / id : {}", waiting.getUser().getName(), id);
 
-        model.addAttribute("currentMenu", "one");
-        return "redirect:/learning_core/mentor/manage/" + waiting.getBoard().getId();
+        model.addAttribute("sidebarType", sidebarType);
+        model.addAttribute("currentMenu", boardType);
+        return "redirect:/team/manage/" + waiting.getBoard().getId() + "?sidebarType=" + sidebarType + "&boardType=" + boardType;
     }
 
     @PostMapping("/expulsion/{id}")
-    public String teamExpulsion(@PathVariable long id, Model model)
+    public String teamExpulsion(@PathVariable long id, Model model, @RequestParam String sidebarType, @RequestParam String boardType)
     {
         Optional<Team> findTeam = teamService.findById(id);
-        if(findTeam.isEmpty()) return "redirect:/learning_core/mentor";
+        if(findTeam.isEmpty()) return "redirect:/" + boardType;
 
         Team team = findTeam.get();
         teamService.expulsionTeam(team);
 
-        model.addAttribute("currentMenu", "one");
-        return "redirect:/learning_core/mentor/manage/" + team.getBoard().getId();
+        model.addAttribute("sidebarType", sidebarType);
+        model.addAttribute("currentMenu", boardType);
+        return "redirect:/team/manage/" + team.getBoard().getId() + "?sidebarType=" + sidebarType + "&boardType=" + boardType;
     }
 }
