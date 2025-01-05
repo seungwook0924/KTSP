e21579f93dfaf78ce5bd142fff61ce3f52979302
SeungWook_Lee <leesw_0924@icloud.com>
2025-01-05 20:55:25 +0900

    postgresql->mysql 준비

--- Database/init.sql
+++ Database/init.sql
@@ -81,6 +81,3 @@ CREATE TABLE file_mapping (
                               path VARCHAR(255) NOT NULL,
                               extension VARCHAR(20) NOT NULL
 );
-
-INSERT INTO users VALUES
-    (1, 'admin', 201921321, '$2a$10$kBQhpciRe.xAY1MpT6OKveoF5d5SVoc2/HzSG9peXoY9uYJo.pqYK', 'senior', '이승욱', '010-5325-2904', 'AI', 4.2, 'lso_0924@kangwon.ac.kr');
