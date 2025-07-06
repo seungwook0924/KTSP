21c8de9041e4e5e5b3ea789006ce9721d39d6eb6
Lee seungwook <leesw_0924@icloud.com>
2025-07-06 20:11:36 +0900

    백엔드 프로젝트 재생성

---
+++ SpringBoot/src/main/java/com/seungwook/ktsp/KtspApplication.java
@@ -0,0 +1,13 @@
+package com.seungwook.ktsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtspApplication.class, args);
+    }
+
+}
